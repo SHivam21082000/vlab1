c341454af0237977dada3a04ce719ff5d2e1e44c
SHivam21082000 <shivamshukla752@gmail.com>
2021-10-27 23:05:14 +0530

    partial work done
    partial simulation done

--- sine.js
+++ sine.js
@@ -1,4 +1,19 @@
-var x = document.getElementById("resistor-one").value;
+
 function calculate(){
-  console.log(x);
+  var x = document.getElementById("resistor-one").value;
+  var y = document.getElementById("resistor-two").value;  
+  var r1 = parseInt(x);
+  var r2 = parseInt(y);
+  var z = 15*r1/(r1+r2); 
+  document.getElementById("demo").innerHTML = z;
+  document.getElementById("demo1").innerHTML = -z;
+}
+
+function reset(){
+  document.getElementById("resistor-one").value = '';
+  document.getElementById("resistor-two").value = '';
+  document.getElementById("demo").innerHTML = '';
+  document.getElementById("demo1").innerHTML = '';
+  document.getElementById("freq").value = '';
+  document.getElementById("input-voltage").value = '';
 }
